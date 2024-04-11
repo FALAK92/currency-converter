7d7220295d60229ddb0e92890a6eae0e72cb32e7
FALAK92 <naaz92falak@gmail.com>
2024-04-11 17:37:53 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency: any = {
+  USD: 1,
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+
+let user_Answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter your Amount",
+    type: "number",
+  },
+]);
+
+let fromAmout = currency[user_Answer.from];
+let toAmount = currency[user_Answer.to];
+let amount = user_Answer.amount;
+let baseAmount = amount / fromAmout;
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.round(convertedAmount));
